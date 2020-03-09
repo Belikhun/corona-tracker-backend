8f2db6273cbd164f1d03a81a8fc966ab8a464fc1
Belikhun <belivipro9x99@gmail.com>
2020-03-09 12:22:18 +0700

    🥓 add files

---
+++ api/corona.php
@@ -0,0 +1,134 @@
+<?php
+	require_once $_SERVER["DOCUMENT_ROOT"] ."/lib/ratelimit.php";
+	require_once $_SERVER["DOCUMENT_ROOT"] ."/lib/belibrary.php";
+	require_once $_SERVER["DOCUMENT_ROOT"] ."/lib/cache.php";
+
+	define("PAGE_TYPE", "API");
+
+	$default = Array(
+        "global" => Array(
+			"confirmed" => 0,
+			"deaths" => 0,
+			"recovered" => 0,
+			"last" => Array(
+				"confirmed" => 0,
+				"deaths" => 0,
+				"recovered" => 0,
+			),
+			"update" => 0
+		),
+
+		"vietnam" => Array(
+			"list" => Array(),
+			"confirmed" => 0,
+			"deaths" => 0,
+			"recovered" => 0,
+			"update" => 0
+		)
+	);
+
+	$cache = new cache("api.data", $default);
+	$cache -> setAge(30);
+		
+	if ($cache -> validate()) {
+		$returnData = $cache -> getData();
+		stop(0, "Fetched from Cache", 200, $returnData, true);
+	}
+
+	$url = "https://corona-api.kompa.ai/graphql";
+	$data = '{"operationName":"countries","variables":{},"query":"\nquery countries {\ncountries {\nCountry_Region\nConfirmed\nDeaths\nRecovered\nLast_Update\n__typename\n}\n\nprovinces {\nProvince_Name\nProvince_Id\nConfirmed\nDeaths\nRecovered\nLast_Update\n__typename\n}\n\ntotalConfirmedLast\ntotalDeathsLast\ntotalRecoveredLast\n}\n"}';
+	
+	$header = Array(
+		":authority: corona-api.kompa.ai",
+		":method: POST",
+		":path: /graphql",
+		":scheme: https",
+		"Accept: */*",
+		"Accept-Encoding: gzip, deflate, br",
+		"Accept-Language: vi,en-US;q=0.9,en;q=0.8,vi-VN;q=0.7",
+		"Cache-Control: no-cache",
+		"Connection: keep-alive",
+		"Content-Length: ". strlen($data),
+		"content-type: application/json",
+		"Origin: https://corona.kompa.ai",
+		"Pragma: no-cache",
+		"Referer: https://corona.kompa.ai/",
+		"Sec-Fetch-Dest: empty",
+		"Sec-Fetch-Mode: cors",
+		"Sec-Fetch-Site: same-site",
+		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+	);
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_ENCODING, "");
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	$response = curl_exec($ch);
+
+	if ($code = curl_errno($ch))
+		stop($code, "Request Error: ". curl_error($ch), 500);
+
+	$data = json_decode($response, true);
+
+	if ($data["data"]) {
+		$global = $default["global"];
+		$vietnam = $default["vietnam"];
+
+		// GLOBAL DATA
+
+		$global["last"]["confirmed"] += (int)$data["data"]["totalConfirmedLast"];
+		$global["last"]["deaths"] += (int)$data["data"]["totalDeathsLast"];
+		$global["last"]["recovered"] += (int)$data["data"]["totalRecoveredLast"];
+
+		foreach ($data["data"]["countries"] as $value) {
+			$global["confirmed"] += (int)$value["Confirmed"];
+			$global["deaths"] += (int)$value["Deaths"];
+			$global["recovered"] += (int)$value["Recovered"];
+			$lu = ((int)$value["Last_Update"]) / 1000;
+
+			if ($lu > $global["update"])
+				$global["update"] = $lu;
+		}
+
+		// VIETNAM DATA
+
+		foreach ($data["data"]["provinces"] as $value) {
+			$vietnam["confirmed"] += (int)$value["Confirmed"];
+			$vietnam["deaths"] += (int)$value["Deaths"];
+			$vietnam["recovered"] += (int)$value["Recovered"];
+			$lu = ((int)$value["Last_Update"]) / 1000;
+
+			if ($lu > $vietnam["update"])
+				$vietnam["update"] = $lu;
+
+			$item = Array(
+				"name" => $value["Province_Name"],
+				"update" => ((int)$value["Last_Update"]) / 1000,
+				"confirmed" => (int)$value["Confirmed"],
+				"deaths" => (int)$value["Deaths"],
+				"recovered" => (int)$value["Recovered"]
+			);
+
+			array_push($vietnam["list"], $item);
+		}
+
+		usort($vietnam["list"], function($a, $b) {
+			$a = $a["confirmed"];
+			$b = $b["confirmed"];
+		
+			if ($a === $b)
+				return 0;
+	
+			return ($a > $b) ? -1 : 1;
+		});
+
+		$data = Array( "global" => $global, "vietnam" => $vietnam );
+		$cache -> save($data);
+	}
+
+	stop(0, "Request Completed", curl_getinfo($ch, CURLINFO_HTTP_CODE), $data);
---
+++ data/info.php
@@ -0,0 +1,33 @@
+<?php
+    //? |-----------------------------------------------------------------------------------------------|
+    //? |  /data/info.php                                                                               |
+    //? |                                                                                               |
+    //? |  Copyright (c) 2018-2020 Belikhun. All right reserved                                         |
+    //? |  Licensed under the MIT License. See LICENSE in the project root for license information.     |
+    //? |-----------------------------------------------------------------------------------------------|
+
+	// dont claim it for your own. its not nice
+	define("APPNAME", "Corona status API");
+	define("AUTHOR", "Belikhun");
+	define("VERSION", "1.0.0");
+	define("VERSION_TAG", "release");
+	define("REPO_ADDRESS", "");
+	define("REPORT_ERROR", REPO_ADDRESS . "/issues");
+	define("CONTACT_LINK", "http://m.me/belivipro9x99");
+	define("TRACK_ID", "");
+
+	// Size Limit
+	define("MAX_UPLOAD_SIZE", 10*1024*1024);
+	define("MAX_IMAGE_SIZE", 2097153);
+	define("MAX_ATTACHMENT_SIZE", 268435456);
+	
+	define("UPLOAD_ALLOW", Array("pas", "cpp", "c", "pp", "exe", "class", "py", "java"));
+	define("IMAGE_ALLOW", Array("jpg", "png", "gif", "webp"));
+
+	// Path
+	define("AVATAR_DIR", $_SERVER["DOCUMENT_ROOT"] ."/data/avatar");
+	define("PROBLEM_DIR", $_SERVER["DOCUMENT_ROOT"] ."/data/problems");
+
+	// CACHE
+	define("CACHE_LOCATION", $_SERVER["DOCUMENT_ROOT"] ."/.cache");
+	define("CACHE_DEFAULT_AGE", 10);
